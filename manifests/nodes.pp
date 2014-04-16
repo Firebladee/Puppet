1d78939282768dda21898ba4dcb7ace5bc14a7e3
Mark Greenhalgh <mgreenha@yahoo.co.uk>
2014-04-16 15:33:41 +0100

    Moved code from nodes to module

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,50 +4,7 @@ include vagrant
 
   Yumrepo <| |> -> Package <| provider != 'rpm' |>
 
-#  $omd_site = 'test'
-#  include omd::install
-#  include omd::site::add
-
   class {'yum':
     extrarepo => ['omd','epel','puppetlabs',]
-  }
-
-  if $hostname == 'puppet' {
-    package { 'puppet-server': ensure => installed, }
-    class { 'puppetdb':
-      node_purge_ttl => '2d',
-      node_ttl       => '2d',
-      listen_address => '0.0.0.0',
-    }
-    class { 'puppetdb::master::config': 
-      restart_puppet => false,
-    }
-    service {'puppetmaster':
-      ensure => running,
-      enable => true,
-    }
-
-    Ini_setting{
-      section => 'main',
-      path    => '/etc/puppet/puppet.conf',
-      ensure  => present,
-    }
-    ini_setting { 'puppet.conf/main/modulepath':
-      setting => 'modulepath',
-     value   => '/vagrant/modules',
-    }
-    ini_setting { 'puppet.conf/main/manifestdir':
-      setting => 'manifestdir',
-      value   => '/vagrant/manifests',
-    }
-#    Ini_setting{
-#      section => 'agent',
-#      path    => '/etc/puppet/puppet.conf',
-#      ensure  => present,
-#    }
-    ini_setting { 'puppet.conf/main/server':
-      setting => 'server',
-      value   => 'puppet.example.com',
-    }
   }
 }
--- modules/vagrant/manifests/init.pp
+++ modules/vagrant/manifests/init.pp
@@ -8,4 +8,47 @@ class vagrant {
     content => template('vagrant/hosts.erb'),
   }
 
+  if $hostname == 'puppet' {
+    package { 'puppet-server': ensure => installed, }
+    class { 'puppetdb':
+      node_purge_ttl => '2d',
+      node_ttl       => '2d',
+      listen_address => '0.0.0.0',
+    }
+    class { 'puppetdb::master::config':
+      restart_puppet => false,
+    }
+    service {'puppetmaster':
+      ensure => running,
+      enable => true,
+    }
+
+    Ini_setting{
+      section => 'main',
+      path    => '/etc/puppet/puppet.conf',
+      ensure  => present,
+    }
+    ini_setting { 'puppet.conf/main/modulepath':
+      setting => 'modulepath',
+     value   => '/vagrant/modules',
+    }
+    ini_setting { 'puppet.conf/main/manifestdir':
+      setting => 'manifestdir',
+      value   => '/vagrant/manifests',
+    }
+    ini_setting { 'puppet.conf/main/server':
+      setting => 'server',
+      value   => 'puppet.example.com',
+    }
+  } else {
+    Ini_setting{
+      section => 'agent',
+      path    => '/etc/puppet/puppet.conf',
+      ensure  => present,
+    }
+    ini_setting { 'puppet.conf/main/server':
+      setting => 'server',
+      value   => 'puppet.example.com',
+    }
+  }
 }
