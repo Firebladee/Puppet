a611a0ce296abb76c1ce7d1c8974e55fdf72beaa
Mark Greenhalgh <mgreenha@yahoo.co.uk>
2014-05-12 13:19:15 +0100

    Update submodules

--- modules/yum/manifests/repo/remi.pp
+++ modules/yum/manifests/repo/remi.pp
@@ -2,11 +2,11 @@
 
 class yum::repo::remi {
   yum::managed_yumrepo { 'remi':
-    descr    => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch',
-    baseurl  => 'http://rpms.famillecollet.com/el$releasever.$basearch/',
-    enabled  => 1,
-    gpgcheck => 1,
-    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
-    priority => 1,
+    descr      => 'Les RPM de remi pour Enterpise Linux $releasever - $basearch',
+    mirrorlist => 'http://rpms.famillecollet.com/enterprise/$releasever/remi/mirror',
+    enabled    => 1,
+    gpgcheck   => 1,
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
+    priority   => 1,
   }
 }
